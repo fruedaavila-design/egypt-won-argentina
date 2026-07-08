3d105cfd6a25ee3662b25a1b79e455fd9842e972
fruedaavila-design <frueda.avila@gmail.com>
2026-07-08 01:19:56 -0600

    Create Hero.js

---
+++ components/Hero.js
@@ -0,0 +1,23 @@
+export default function Hero() {
+  return (
+    <section className="ewaHero">
+      <p className="ewaCaseLabel">CASE FILE · EWA-2026-001</p>
+
+      <h1>
+        EGYPT
+        <span>WON</span>
+        ARGENTINA
+      </h1>
+
+      <p className="ewaHeroSubtitle">
+        We’re taking FIFA to court.
+        <br />
+        Egypt wants the win back.
+      </p>
+
+      <a className="ewaHeroButton" href="/sign">
+        Sign the Case
+      </a>
+    </section>
+  );
+}
