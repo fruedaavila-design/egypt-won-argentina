87038bd7a1fdf6f90fbd0b5fab337b3203391059
fruedaavila-design <frueda.avila@gmail.com>
2026-07-07 23:41:50 -0600

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <b>Egypt Won Argentina</b>
+      <p>Independent fan-made Internet Verdict. Not affiliated with FIFA, Argentina, Egypt or any official organization.</p>
+    </footer>
+  );
+}
