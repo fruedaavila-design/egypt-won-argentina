7288feaca11850c9c6ca3ba89c98890ca910cf10
fruedaavila-design <frueda.avila@gmail.com>
2026-07-07 23:41:36 -0600

    Create StatusBar.js

---
+++ components/StatusBar.js
@@ -0,0 +1,10 @@
+export default function StatusBar() {
+  return (
+    <div className="statusBar">
+      <span>CASE STATUS: <b>ACTIVE</b></span>
+      <span>VERIFIED SIGNATURES: <b>24,381</b></span>
+      <span>COUNTRIES: <b>67</b></span>
+      <span>CASE ID: <b>EWA-2026-001</b></span>
+    </div>
+  );
+}
